8494228d85eafeb085bd8f6ceb688ac7ceab8507
sylee <sylee6529@gmail.com>
2024-01-01 23:34:23 +0900

    Initial commit

---
+++ src/main/java/com/jungle/jungleboard/JungleBoardApplication.java
@@ -0,0 +1,13 @@
+package com.jungle.jungleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JungleBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jungle/jungleboard/JungleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jungle.jungleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
