00c9b75628012359865bed3bb3243249eff5f19d
sylee <sylee6529@gmail.com>
2024-01-08 00:53:20 +0900

    feat: Time 관련 annotation 사용 위한 설정 추가 (#1)

---
+++ src/main/java/com/jungle/board/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.jungle.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+}
