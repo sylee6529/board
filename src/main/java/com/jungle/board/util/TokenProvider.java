06ee7f7251e28a45b4590b4e2e59809bd33262e7
sylee <sylee6529@gmail.com>
2024-01-09 01:37:56 +0900

    feat: JWT, Security 설정 (#1)

--- src/main/java/com/jungle/board/util/TokenProvider.java
+++ src/main/java/com/jungle/board/util/TokenProvider.java
@@ -1,42 +1,111 @@
 package com.jungle.board.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import com.jungle.board.user.domain.User;
 
+import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
-@Slf4j
 @Service
 public class TokenProvider {
-    private final String SECRET_KEY;
 
-    public TokenProvider(@Value("${jwt.secret-key}") String secretKey) {
-        this.SECRET_KEY = secretKey;
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+
+    /** 토큰으로부터 유저 아이디(변수명만 userName) 추출 **/
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
-    public String create(User user) {
-        Date expiredAt = Date.from(
-                LocalDateTime
-                        .now()
-                        .atZone(ZoneId.systemDefault())
-                        .toInstant()
-                        .plus(1, ChronoUnit.DAYS)
-        );
+    /** 토큰으로부터 클레임 정보 추출 **/
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
-        return Jwts.builder()
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
-                .setSubject(user.getId().toString())
-                .setIssuer("jungleboard")
-                .setIssuedAt(new Date())
-                .setExpiration(expiredAt)
+    /** extraClaims 없이 토큰 생성 **/
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    /** 토큰 생성 **/
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return buildToken(extraClaims, userDetails, jwtExpiration);
+    }
+
+    /** refresh 토큰 생성 **/
+    public String generateRefreshToken(
+            UserDetails userDetails
+    ) {
+        return buildToken(new HashMap<>(), userDetails, refreshExpiration);
+    }
+
+
+    private String buildToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails,
+            long expiration
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    /** 토큰 유효성 검사 **/
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    /** 토큰 만료 시간 검사 **/
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    /** 토큰 만료 시간 추출 **/
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    /** 모든 클레임 추출 **/
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /** 서명 key 생성 **/
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
