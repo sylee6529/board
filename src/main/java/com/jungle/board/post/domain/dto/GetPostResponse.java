c40eeb90403a9cff2d16855adf84ff73c75bd89d
sylee <sylee6529@gmail.com>
2024-01-10 02:40:57 +0900

    refactor: response에 time 필드 추가 (#3)

--- src/main/java/com/jungle/board/post/domain/dto/GetPostResponse.java
+++ src/main/java/com/jungle/board/post/domain/dto/GetPostResponse.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -19,6 +20,8 @@ public class GetPostResponse {
     private Long boardId;
     private String boardName;
     private String userNickname;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
     private List<GetCommentDto> comments;
 
     public static GetPostResponse from(Post post) {
@@ -31,6 +34,8 @@ public class GetPostResponse {
                 post.getBoard().getId(),
                 post.getBoard().getName(),
                 post.getUser().getNickname(),
+                post.getCreatedAt(),
+                post.getUpdatedAt(),
                 post.getComments().stream()
                         .map(GetCommentDto::from)
                         .toList()
